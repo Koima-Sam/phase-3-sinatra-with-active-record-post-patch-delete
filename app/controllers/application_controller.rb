dfb4df5264a6600f216a27106f1439fe081305d3
Koima-Sam <sam17koima@gmail.com>
2022-09-05 15:02:05 +0300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,21 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+  # Removes specified item by id
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  # add new review to the db
+  post '/reviews' do
+    new_review = Review.create("comment": params[:comment], "score": params[:score], "game_id": params[:game_id], "user_id":params[:user_id])
+    new_review.to_json
+  end 
+  patch '/reviews/:id' do
+    new_review = Review.update("comment": params[:comment], "score": params[:score])
+    Review.find(params[:id]).to_json
+  end
 
 end
